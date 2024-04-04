c209b42b28d1723ca6381ed83bcf72f740b903c5
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2024-04-04 10:52:40 -0300

    feat: WP-02 => Criando model de Product

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -2,8 +2,14 @@
 {
     public class Category
     {
-        public int Id { get; set; }
-
+        public Guid Id { get; set; }
         public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string EditedBy { get; set;}
+        public DateTime  EditedOn { get; set; }
+
+
+
     }
 }
